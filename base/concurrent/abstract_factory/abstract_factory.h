375b0047ea38c4d51837db48d82f092cb8cecb5c
qianqians <anxiangqq@gmail.com>
2013-08-28 00:08:20 +0800

    修改abstract_factory接口

--- base/concurrent/abstract_factory/abstract_factory.h
+++ base/concurrent/abstract_factory/abstract_factory.h
@@ -52,7 +52,7 @@ public:
 		_Allocator_mirco_pool.deallocate(_mirco_pool, count);
 	}
 
-	boost::shared_ptr<T> create_product(){
+	T * create_product(){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -72,13 +72,12 @@ public:
 		}
 
 		new(pT) T();
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1>
-	boost::shared_ptr<T> create_product(t1 _t1){
+	T * create_product(t1 _t1){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -98,13 +97,12 @@ public:
 		}
 
 		new(pT) T(_t1);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2){
+	T * create_product(t1 _t1, t2 _t2){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -124,13 +122,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -150,13 +147,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -176,13 +172,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -202,13 +197,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5, typename t6>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -228,13 +222,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5, _t6);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5, typename t6, typename t7>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -254,13 +247,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5, _t6, _t7);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5, typename t6, typename t7, typename t8>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -280,13 +272,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5, _t6, _t7, _t8);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5, typename t6, typename t7, typename t8, typename t9>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8, t9 _t9){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8, t9 _t9){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -306,13 +297,12 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5, _t6, _t7, _t8, _t9);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
 		
-		return _shared_ptr;
+		return pT;
 	}
 
 	template <typename t1, typename t2, typename t3, typename t4, typename t5, typename t6, typename t7, typename t8, typename t9, typename t10>
-	boost::shared_ptr<T> create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8, t9 _t9, t10 _t10){
+	T * create_product(t1 _t1, t2 _t2, t3 _t3, t4 _t4, t5 _t5, t6 _t6, t7 _t7, t8 _t8, t9 _t9, t10 _t10){
 		T * pT = 0;
 		for(int i = 0; i < count; i++){
 			boost::mutex::scoped_lock lock(_mirco_pool[i]._mu, boost::try_to_lock);
@@ -332,12 +322,10 @@ public:
 		}
 
 		new(pT) T(_t1, _t2, _t3, _t4, _t5, _t6, _t7, _t8, _t9, _t10);
-		boost::shared_ptr<T> _shared_ptr(_mirco_pool, boost::bind(&abstract_factory::release_product, this, _1));
-		
-		return _shared_ptr;
+
+		return pT;
 	}
 
-private:
 	void release_product(T * pT){
 		pT->~T();
 
